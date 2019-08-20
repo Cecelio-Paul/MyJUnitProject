eadbaf6fed19f75f12a52dd45579e3c9a5d7a300
David <david.paul@academytrainee.com>
2019-08-20 13:13:28 +0100

    Initial commit

---
+++ src/main/java/com/qa/App.java
@@ -0,0 +1,16 @@
+package com.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println(args[0]);
+        System.out.println( "Hello World!" );
+    }
+
+}
